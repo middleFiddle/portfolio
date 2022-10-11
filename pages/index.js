38e129c5dcb37c2698f24753a64921ac02ac19f9
Ryan Gregory <ryan.middleFiddle@gmail.com>
2022-10-10 23:40:42 -0400

    contentlayer code commented out due to build errors

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head"
 import Image from "next/image"
 import { useState, useRef, useEffect } from "react"
-import { allPosts } from "@contentlayer/generated"
+//import { allPosts } from "contentlayer/generated"
 import Link from "next/link"
 import ProjectCard from "../components/ProjectCard"
 import TitleCard from "../components/TitleCard"
 import CallToAction from "../components/CallToAction"
 import Navbar from "../components/Navbar"
-
+/* 
 export async function getStaticProps() {
   const posts = allPosts
 
@@ -16,19 +16,23 @@ export async function getStaticProps() {
       posts,
     },
   }
-}
+} */
 
-const titles = [
+/* const titles = [
   "Intro",
   "Tell Me More",
   "Interview Questions",
   "Personal History",
   "I'm Complicated",
   "Atlanta Violins Project",
-]
+] */
 
-export default function Home({ posts }) {
-  const getText = (t) => {
+export default function Home(
+  {
+    /*posts*/
+  }
+) {
+  /*   const getText = (t) => {
     return posts.find((p) => p.title == t)
   }
 
@@ -40,7 +44,7 @@ export default function Home({ posts }) {
         dangerouslySetInnerHTML={{ __html: getText(t).body.html }}
       ></div>
     )
-  }
+  } */
 
   return (
     <div className="bg-slate-900 min-h-screen ">
@@ -60,7 +64,19 @@ export default function Home({ posts }) {
             id="text-box"
             className="font-serif text-lg leading-relaxed tracking-widest opacity-100 text-indigo-50"
           >
-            {readMe("Intro")}
+            <p>
+              I'm a web developer from Atlanta 🍑. I focus on enabling
+              contractors, artists, and small businesses to expand their
+              capacity to serve clients and customers, while reducing the burden
+              of repetitive tasks that can create bottlenecks, especially for
+              self-employed people.
+            </p>
+            <p>
+              My strongest stack is full stack Typescript development with React
+              and Tailwind CSS on the front end, MySQL and Prisma handling data,
+              with routing, API endpoints, and fine-grained control over
+              server/client rendering with Next.js
+            </p>
           </div>
           <CallToAction></CallToAction>
         </section>
@@ -69,7 +85,11 @@ export default function Home({ posts }) {
           <h1 className="text-rose-300 font-mono text-4xl font-thin tracking-wide">
             Projects
           </h1>
-          <ProjectCard readMe={readMe("Atlanta Violins Project")}></ProjectCard>
+          <ProjectCard
+            readMe={
+              "This project began as a replacement for a static landing page is currently being developed into a full-stack application.Built with React and Tailwind in Next.js with Prisma, MySQL through Planetscale, and S3."
+            }
+          ></ProjectCard>
         </section>
       </div>
       {/*       <footer className={styles.footer}>
