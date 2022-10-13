8ecf363e513ff756c03ded886cc6e39db19771a8
Ryan Gregory <ryan.middleFiddle@gmail.com>
2022-10-13 15:30:06 -0400

    simple  requirement validation on client complete

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -26,8 +26,29 @@ const ContactForm = () => {
         <label className="label">
           <span className="label-text">What{`\u0027`}s your name?</span>
         </label>
+        {errors.name?.type === "required" && (
+          <div
+            role="alert"
+            className="alert alert-error w-fit h-fit text-sm py-1 justify-center text-indigo-800 font-mono "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>My name is Ryan! What{"\u0027"}s yours?</span>
+          </div>
+        )}
         <input
-          {...register("name")}
+          {...register("name", { required: true })}
           type="text"
           className="input"
           placeholder="{ Your Name }"
@@ -91,14 +112,37 @@ const ContactForm = () => {
             />
           </label>
         </div>
-        <label>
+        <label className="label">
           <span className="label-text">
             Add your email to receive my resume and a friendly introduction!
           </span>
         </label>
+        {errors.email?.type === "required" && (
+          <div
+            role="alert"
+            className="alert alert-error w-fit h-fit text-sm py-1 justify-center text-indigo-800 font-mono "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>We need your email address!</span>
+          </div>
+        )}
         <input
-          {...register("email")}
-          type="text"
+          {...register("email", {
+            required: true,
+          })}
+          type="email"
           className="input"
           placeholder="{ email@address.com }"
         />
@@ -107,6 +151,27 @@ const ContactForm = () => {
             Pick the format(s) you{"\u0027"}d like:
           </span>
         </label>
+        {errors.format?.type === "required" && (
+          <div
+            role="alert"
+            className="alert alert-error w-fit h-fit text-sm py-1 justify-center text-indigo-800 font-mono "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Pick one or both</span>
+          </div>
+        )}
         <div {...register("format")} className="btn-group space-x-2">
           <label className="label cursor-pointer flex space-x-4">
             <span className="label-text">.DOC</span>
@@ -121,7 +186,7 @@ const ContactForm = () => {
           <label className="label cursor-pointer flex space-x-4">
             <span className="label-text">.PDF</span>
             <input
-              {...register("format")}
+              {...register("format", { required: true })}
               value=".pdf"
               type="checkbox"
               className="toggle"
