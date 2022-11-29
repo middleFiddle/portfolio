25af9741bf7841fdc8270b5b4cb1ba22d5404edc
Ryan Gregory <ryan.middleFiddle@gmail.com>
2022-11-29 13:52:26 -0500

    rather preinstall than postinstall

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,9 @@
 import React from "react"
 import ContactForm from "../components/ContactForm"
-import Script from "next/script"
 
 const contact = () => {
     return (
         <div>
-            <Script src="https://smtpjs.com/v3/smtp.js" />
             <ContactForm></ContactForm>
         </div>
     )
